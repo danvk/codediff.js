6c46d8f25fb16685ce3b83e47ef79d1777605d52
Dan Vanderkam <danvdk@gmail.com>
2014-07-03 00:23:02 -0400

    add back support for "show more" links

--- diffview.js
+++ diffview.js
@@ -6,7 +6,7 @@ var differ = function(beforeText, afterText, userParams) {
 
   var defaultParams = {
     contextSize: 3,
-    syntaxHighlighting: false,
+    language: null,
     beforeName: "Before",
     afterName: "After"
   };
@@ -18,7 +18,7 @@ var differ = function(beforeText, afterText, userParams) {
   var sm = new difflib.SequenceMatcher(this.beforeLines, this.afterLines);
   this.opcodes = sm.get_opcodes();
 
-  if (this.params.syntaxHighlighting) {
+  if (this.params.language) {
     var lang = this.params.language;
     this.beforeLinesHighlighted = differ.highlightText_(beforeText, lang);
     this.afterLinesHighlighted = differ.highlightText_(afterText, lang);
@@ -76,10 +76,15 @@ differ.distributeSpans_ = function(text) {
 differ.highlightText_ = function(text, opt_language) {
   // TODO(danvk): look into suppressing highlighting if .relevance is low.
   var html;
+  // console.log(hljs.highlightAuto(text));
   if (opt_language) {
     html = hljs.highlight(opt_language, text, true).value;
   } else {
-    html = hljs.highlightAuto(text).value;
+    return null;
+    // This produces a lot of false positives:
+    // html = hljs.highlightAuto(text).value;
+    // There is a relevance number but it's hard to threshold. The file
+    // extension is probably a good enough heuristic.
   }
 
   // Some of the <span>s might cross lines, which won't work for our diff
@@ -87,14 +92,77 @@ differ.highlightText_ = function(text, opt_language) {
   return differ.distributeSpans_(html);
 }
 
+/**
+ * Attach event listeners, notably for the "show more" links.
+ */
+differ.prototype.attachHandlers_ = function(el) {
+  // Synchronize horizontal scrolling.
+  var $wrapperDivs = $(el).find('.diff-wrapper');
+  $wrapperDivs.on('scroll', function(e) {
+    var otherDiv = $wrapperDivs.not(this).get(0);
+    otherDiv.scrollLeft = this.scrollLeft;
+  }.bind(this));
+
+  var this_differ = this;
+  $(el).on('click', '.skip a', function(e) {
+    e.preventDefault();
+    var skipData = $(this).closest('.skip').data();
+    var beforeIdx = skipData.beforeStartIndex;
+    var afterIdx = skipData.afterStartIndex;
+    var jump = skipData.jumpLength;
+    var beforeEnd = beforeIdx + jump;
+    var afterEnd = afterIdx + jump;
+    var change = "equal";
+    var newRows = [];
+    for (var i = 0; i < jump; i++) {
+      var data = this_differ.buildRow_(beforeIdx, beforeEnd, afterIdx, afterEnd, change);
+      beforeIdx = data.newBeforeIdx;
+      afterIdx = data.newAfterIdx;
+      newRows.push(data.row);
+    }
+
+    // Replace the "skip" rows with real code.
+    var $lefts = $(this).closest('.diff').find('.diff-left-line-no, .diff-left-content').find('[line-no=' + (1+skipData.beforeStartIndex) + ']');
+    var $rights = $(this).closest('.diff').find('.diff-right-line-no, .diff-right-content').find('[line-no=' + (1+skipData.afterStartIndex) + ']');
+
+    if ($lefts.length == 2 && $rights.length == 2) {
+      var els = [$lefts.get(0), $lefts.get(1), $rights.get(0), $rights.get(1)];
+      for (var rowIdx = newRows.length - 1; rowIdx >= 0; rowIdx--) {
+        var row = newRows[rowIdx];
+        for (var i = 0; i < row.length; i++) {
+          $(els[i]).after(row[i]);
+        }
+      }
+      els.forEach(function(el) { $(el).remove(); });
+    }
+  });
+};
+
+differ.prototype.buildRow_ = function(beforeIdx, beforeEnd, afterIdx, afterEnd, change) {
+  // TODO(danvk): move this logic into addCells() or get rid of it.
+  var beforeLines = this.params.language ? this.beforeLinesHighlighted : this.beforeLines;
+  var afterLines = this.params.language ? this.afterLinesHighlighted : this.afterLines;
+
+  var els = [];
+  beforeIdx = addCells(els, beforeIdx, beforeEnd, this.params.language, beforeLines, 'before ' + change, beforeIdx + 1);
+  afterIdx = addCells(els, afterIdx, afterEnd, this.params.language, afterLines, 'after ' + change, afterIdx + 1);
+
+  if (change == 'replace') {
+    differ.addCharacterDiffs_(els[1], els[3], this.params.language);
+  }
+
+  return {
+    row: els,
+    newBeforeIdx: beforeIdx,
+    newAfterIdx: afterIdx
+  };
+};
+
 differ.prototype.buildView_ = function() {
   var $leftLineDiv = $('<div class="diff-line-no diff-left-line-no">');
   var $leftContent = $('<div class="diff-content diff-left-content">');
   var $rightLineDiv = $('<div class="diff-line-no diff-right-line-no">');
   var $rightContent = $('<div class="diff-content diff-right-content">');
-
-  var beforeLines = this.params.syntaxHighlighting ? this.beforeLinesHighlighted : this.beforeLines;
-  var afterLines = this.params.syntaxHighlighting ? this.afterLinesHighlighted : this.afterLines;
 
   var contextSize = this.params.contextSize;
   var rows = [];
@@ -124,20 +192,21 @@ differ.prototype.buildView_ = function() {
           var els = [];
           topRows.push(els);
 
-          var $skipEl = $('<div class=skip><a href="#">Show ' + jump + ' lines</a></div>');
+          var $skipEl = $('<div class="skip code"><a href="#">Show ' + jump + ' lines</a></div>');
           $skipEl.data({
             'beforeStartIndex': beforeIdx,
             'afterStartIndex': afterIdx,
-            'jumpLength': jump
-          });
+            'jumpLength': jump,
+          }).attr('line-no', 1 + afterIdx);
           
+          els.push($('<div class=line-no>...</div>').attr('line-no', 1+beforeIdx).get(0));
+          els.push($('<div class="skip code">...</div>').attr('line-no', 1+beforeIdx).get(0));
+          els.push($('<div class=line-no>...</div>').attr('line-no', 1+afterIdx).get(0));
+          els.push($skipEl.get(0));
+
           beforeIdx += jump;
           afterIdx += jump;
           i += jump - 1;
-          els.push($('<div>...</div>').get(0));
-          els.push($('<div class=skip>...</div>').get(0));
-          els.push($('<div>...</div>').get(0));
-          els.push($skipEl.get(0));
           
           // skip last lines if they're all equal
           if (isEnd) {
@@ -148,14 +217,10 @@ differ.prototype.buildView_ = function() {
         }
       }
 
-      var els = [];
-      topRows.push(els);
-      beforeIdx = addCells(els, beforeIdx, beforeEnd, this.params.syntaxHighlighting, beforeLines, 'before line-' + (beforeIdx + 1) + ' ' + change);
-      afterIdx = addCells(els, afterIdx, afterEnd, this.params.syntaxHighlighting, afterLines, 'after line-' + (afterIdx + 1) + ' ' + change);
-
-      if (change == 'replace') {
-        differ.addCharacterDiffs_(els[1], els[3], this.params.syntaxHighlighting);
-      }
+      var data = this.buildRow_(beforeIdx, beforeEnd, afterIdx, afterEnd, change);
+      beforeIdx = data.newBeforeIdx;
+      afterIdx = data.newAfterIdx;
+      topRows.push(data.row);
     }
 
     for (var i = 0; i < topRows.length; i++) rows.push(topRows[i]);
@@ -183,20 +248,19 @@ differ.prototype.buildView_ = function() {
     $rightContent.append(row[3]);
   });
 
-  var $wrapperDivs = $container.find('.diff-wrapper');
-  $wrapperDivs.on('scroll', function(e) {
-    var otherDiv = $wrapperDivs.not(this).get(0);
-    otherDiv.scrollLeft = this.scrollLeft;
-  });
+  this.attachHandlers_($container);
 
   return $container.get(0);
 };
 
-function addCells(row, tidx, tend, isHtml, textLines, change) {
+function addCells(row, tidx, tend, isHtml, textLines, change, line_no) {
   if (tidx < tend) {
     var txt = textLines[tidx].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0");
-    row.push($('<div class=line-no>').text(tidx + 1).get(0));
-    var $code = $('<div>').addClass(change + ' code');
+    row.push($('<div class=line-no>')
+                  .text(tidx + 1)
+                  .attr('line-no', line_no)
+                  .get(0));
+    var $code = $('<div>').addClass(change + ' code').attr('line-no', line_no);
     if (isHtml) {
       $code.html(txt);
     } else {
@@ -205,8 +269,8 @@ function addCells(row, tidx, tend, isHtml, textLines, change) {
     row.push($code.get(0));
     return tidx + 1;
   } else {
-    row.push($('<div class=line-no>').get(0));
-    row.push($('<div class="empty code">').get(0));
+    row.push($('<div class=line-no>').attr('line-no', line_no).get(0));
+    row.push($('<div class="empty code">').attr('line-no', line_no).get(0));
     return tidx;
   }
 }
