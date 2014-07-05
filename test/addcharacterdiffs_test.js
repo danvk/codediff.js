d44031ef885681c2b53a1daf2d5cd2ca05b00aad
Dan Vanderkam <danvdk@gmail.com>
2014-07-05 11:00:04 -0400

    Only wrap balanced DOM subtrees in <span>..</span> for char diffs

--- test/addcharacterdiffs_test.js
+++ test/addcharacterdiffs_test.js
@@ -1,3 +1,23 @@
+QUnit.test('simplifyCodes', function(assert) {
+  assert.deepEqual(
+    codediff.simplifyCodes_([[null, 0, 2], ['x', 2, 4]]),
+                            [[null, 0, 2], ['x', 2, 4]]);
+  assert.deepEqual(
+    codediff.simplifyCodes_([['x', 0, 2], ['x', 2, 4]]),
+                            [['x', 0, 4]]);
+  assert.deepEqual(
+    codediff.simplifyCodes_([['x', 0, 2], ['x', 2, 4], ['y', 4, 6]]),
+                            [['x', 0, 4],              ['y', 4, 6]]);
+});
+
+QUnit.test('codesToHtml', function(assert) {
+  var str = 'hello';
+  var map = { getHtmlSubstring: function(a, b) { return str.substring(a, b) } };
+  var codes = [[null, 0, 1], ['x', 1, 3], ['y', 3, 5]];
+  assert.equal(codediff.codesToHtml_(map, codes),
+    'h<span class="char-x">el</span><span class="char-y">lo</span>');
+});
+
 QUnit.test('char diffs -- simple', function(assert) {
   var before = $('<div>').text("    return '' + date.getFullYear();").get(0);
   var after =  $('<div>').text("    return 'xx' + date.getFullYear();").get(0);
@@ -23,7 +43,7 @@ QUnit.test('char diffs with trailing markup', function(assert) {
   assert.equal($(before).text(), beforeText);
   assert.equal($(after).text(), afterText);
   assert.equal($(before).html(), "<q>''</q>");
-  assert.equal($(after).html(), "<q>'<span class=\"char-insert\">xx</span>'</q>");
+  assert.equal($(after).html(), "<q>'</q><span class=\"char-insert\"><q>xx</q></span><q>'</q>");
 });
 
 QUnit.test('char diffs with markup', function(assert) {
@@ -37,5 +57,15 @@ QUnit.test('char diffs with markup', function(assert) {
   assert.equal($(before).text(), beforeText);
   assert.equal($(after).text(), afterText);
   assert.equal($(before).html(), "    <kw>return</kw> <q>''</q> + date.getFullYear();");
-  assert.equal($(after).html(), "    <kw>return</kw> <q>'<span class=\"char-insert\">xx</span>'</q> + date.getFullYear();");
+  assert.equal($(after).html(), "    <kw>return</kw> <q>'</q><span class=\"char-insert\"><q>xx</q></span><q>'</q> + date.getFullYear();");
+});
+
+QUnit.test('mixed inserts and markup', function(assert) {
+  var beforeCode = '<span class="hljs-string">"q"</span>, s';
+  var afterCode =  '<span class="hljs-string">"q"</span><span class="hljs-comment">/*, s*/</span>';
+  var beforeEl = $('<div>').html(beforeCode).get(0);
+  var afterEl =  $('<div>').html(afterCode).get(0);
+
+  codediff.addCharacterDiffs_(beforeEl, afterEl, true);
+  assert.equal(true, true);
 });
--- test/htmlsubstr_test.js
+++ test/htmlsubstr_test.js
@@ -7,9 +7,9 @@ QUnit.test('basic functionality', function(assert) {
   assert.equal(map.getHtmlSubstring(0, 1), 'f');
   assert.equal(map.getHtmlSubstring(0, 2), 'fo');
   assert.equal(map.getHtmlSubstring(0, 3), 'foo');
-  assert.equal(map.getHtmlSubstring(0, 4), 'foo<span>b');
-  assert.equal(map.getHtmlSubstring(0, 5), 'foo<span>ba');
-  assert.equal(map.getHtmlSubstring(0, 6), 'foo<span>bar');
+  assert.equal(map.getHtmlSubstring(0, 4), 'foo<span>b</span>');
+  assert.equal(map.getHtmlSubstring(0, 5), 'foo<span>ba</span>');
+  assert.equal(map.getHtmlSubstring(0, 6), 'foo<span>bar</span>');
   assert.equal(map.getHtmlSubstring(0, 7), 'foo<span>bar</span>b');
   assert.equal(map.getHtmlSubstring(0, 8), 'foo<span>bar</span>ba');
   assert.equal(map.getHtmlSubstring(0, 9), 'foo<span>bar</span>baz');
@@ -21,14 +21,14 @@ QUnit.test('leading/trailing html', function(assert) {
   var map = new codediff.htmlTextMapper(text, html);
 
   assert.equal(map.getHtmlSubstring(0, 0), '');
-  assert.equal(map.getHtmlSubstring(0, 1), '<p>f');
-  assert.equal(map.getHtmlSubstring(0, 2), '<p>fo');
-  assert.equal(map.getHtmlSubstring(0, 3), '<p>foo');
-  assert.equal(map.getHtmlSubstring(0, 4), '<p>foo<span>b');
-  assert.equal(map.getHtmlSubstring(0, 5), '<p>foo<span>ba');
-  assert.equal(map.getHtmlSubstring(0, 6), '<p>foo<span>bar');
-  assert.equal(map.getHtmlSubstring(0, 7), '<p>foo<span>bar</span>b');
-  assert.equal(map.getHtmlSubstring(0, 8), '<p>foo<span>bar</span>ba');
+  assert.equal(map.getHtmlSubstring(0, 1), '<p>f</p>');
+  assert.equal(map.getHtmlSubstring(0, 2), '<p>fo</p>');
+  assert.equal(map.getHtmlSubstring(0, 3), '<p>foo</p>');
+  assert.equal(map.getHtmlSubstring(0, 4), '<p>foo<span>b</span></p>');
+  assert.equal(map.getHtmlSubstring(0, 5), '<p>foo<span>ba</span></p>');
+  assert.equal(map.getHtmlSubstring(0, 6), '<p>foo<span>bar</span></p>');
+  assert.equal(map.getHtmlSubstring(0, 7), '<p>foo<span>bar</span>b</p>');
+  assert.equal(map.getHtmlSubstring(0, 8), '<p>foo<span>bar</span>ba</p>');
   assert.equal(map.getHtmlSubstring(0, 9), '<p>foo<span>bar</span>baz</p>');
 });
 
@@ -38,26 +38,26 @@ QUnit.test('leading/trailing html, fixed right', function(assert) {
   var map = new codediff.htmlTextMapper(text, html);
 
   assert.equal(map.getHtmlSubstring(0, 9), '<p>foo<span>bar</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(1, 9),     'oo<span>bar</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(2, 9),      'o<span>bar</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(3, 9),       '<span>bar</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(4, 9),              'ar</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(5, 9),               'r</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(6, 9),                '</span>baz</p>');
-  assert.equal(map.getHtmlSubstring(7, 9),                        'az</p>');
-  assert.equal(map.getHtmlSubstring(8, 9),                         'z</p>');
+  assert.equal(map.getHtmlSubstring(1, 9),  '<p>oo<span>bar</span>baz</p>');
+  assert.equal(map.getHtmlSubstring(2, 9),   '<p>o<span>bar</span>baz</p>');
+  assert.equal(map.getHtmlSubstring(3, 9),    '<p><span>bar</span>baz</p>');
+  assert.equal(map.getHtmlSubstring(4, 9),     '<p><span>ar</span>baz</p>');
+  assert.equal(map.getHtmlSubstring(5, 9),      '<p><span>r</span>baz</p>');
+  assert.equal(map.getHtmlSubstring(6, 9),                    '<p>baz</p>');
+  assert.equal(map.getHtmlSubstring(7, 9),                     '<p>az</p>');
+  assert.equal(map.getHtmlSubstring(8, 9),                      '<p>z</p>');
   assert.equal(map.getHtmlSubstring(9, 9),                              '');
 });
 
-QUnit.test('small html', function(assert) {
+QUnit.test('small html, all ranges', function(assert) {
   var html = '<q>xx</q>';
   var text = $('<div>').html(html).text();
   var map = new codediff.htmlTextMapper(text, html);
 
   assert.equal(map.getHtmlSubstring(0, 0), '');
-  assert.equal(map.getHtmlSubstring(0, 1), '<q>x');
+  assert.equal(map.getHtmlSubstring(0, 1), '<q>x</q>');
   assert.equal(map.getHtmlSubstring(0, 2), '<q>xx</q>');
-  assert.equal(map.getHtmlSubstring(1, 2), 'x</q>');
+  assert.equal(map.getHtmlSubstring(1, 2), '<q>x</q>');
   assert.equal(map.getHtmlSubstring(2, 2), '');
 });
 
@@ -84,9 +84,9 @@ QUnit.test('consecutive tags', function(assert) {
   var map = new codediff.htmlTextMapper(text, html);
 
   assert.equal(map.getHtmlSubstring(0, 0), '');
-  assert.equal(map.getHtmlSubstring(0, 1), '<a><b>x');
+  assert.equal(map.getHtmlSubstring(0, 1), '<a><b>x</b></a>');
   assert.equal(map.getHtmlSubstring(0, 2), '<a><b>xx</b></a>');
   assert.equal(map.getHtmlSubstring(1, 1), '');
-  assert.equal(map.getHtmlSubstring(1, 2), 'x</b></a>');
+  assert.equal(map.getHtmlSubstring(1, 2), '<a><b>x</b></a>');
   assert.equal(map.getHtmlSubstring(2, 2), '');
 });
