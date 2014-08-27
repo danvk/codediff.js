2a0e523406cf48607ede55fbedce6d2a2c510de8
Dan Vanderkam <danvdk@gmail.com>
2014-08-27 17:36:15 -0400

    sudo install

--- configure-python.sh
+++ configure-python.sh
@@ -2,7 +2,7 @@
 # Configure Python for testing on Travis.
 # You shouldn't ever need to run this on your dev box.
 set -o errexit
-pip install virtualenv
+sudo pip install virtualenv
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
