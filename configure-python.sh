5589c4797b888b97ceed2005fdfd3770ea8ded04
Dan Vanderkam <danvdk@gmail.com>
2014-09-15 14:54:57 -0400

    Try to get a mac on Travis-CI

--- configure-python.sh
+++ configure-python.sh
@@ -2,7 +2,7 @@
 # Configure Python for testing on Travis.
 # You shouldn't ever need to run this on your dev box.
 set -o errexit
-sudo pip install virtualenv
-virtualenv env
-source env/bin/activate
+#sudo pip install virtualenv
+#virtualenv env
+#source env/bin/activate
 pip install -r requirements.txt
