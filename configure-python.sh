786f76db3146bc8edfe1cd6a04ef5e5f9fe8a4cc
Dan Vanderkam <danvdk@gmail.com>
2014-09-17 11:03:12 -0400

    why is it not installing grunt?

--- configure-python.sh
+++ configure-python.sh
@@ -2,8 +2,15 @@
 # Configure Python for testing on Travis.
 # You shouldn't ever need to run this on your dev box.
 set -o errexit
+set -x
 #sudo pip install virtualenv
 #virtualenv env
 #source env/bin/activate
-sudo npm install -g
+
+pwd
+
+echo 'STARTING NPM INSTALL STEP'
+sudo npm install -g $(pwd)
+echo 'DONE WITH NPM STEP'
+
 sudo pip install -r requirements.txt
